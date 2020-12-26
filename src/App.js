108801549ac2b863d7b3df7e7c29fdc09aaa821a
IraCannet <58562850+IraPapsheva@users.noreply.github.com>
2020-12-26 18:51:18 +0100

    html-basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
             <div className="container">
       <h1>Weather App</h1>
+      <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://github.com/IraCannet" target="_blank" rel="noopener noreferrer">
