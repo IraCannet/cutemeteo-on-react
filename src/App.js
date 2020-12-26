93249e3119cfe743d7c11821421a41a43a7d7c16
IraCannet <58562850+IraPapsheva@users.noreply.github.com>
2020-12-26 18:17:29 +0100

    bootstrap-added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+            <div className="container">
       <h1>Weather App</h1>
-
-        	      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/IraCannet" target="_blank" rel="noopener noreferrer">
-          Ira
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/IraCannet/cutemeteo-on-react"
-          target="_blank" rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/IraCannet" target="_blank" rel="noopener noreferrer">
+            Ira
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/IraCannet/cutemeteo-on-react"
+            target="_blank" rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+        </div>
     </div>
   );
 }
