9d83fb164197a375c39d3cf149f2cab8d55b245a
IraCannet <58562850+IraPapsheva@users.noreply.github.com>
2020-12-26 17:28:26 +0100

    added-signature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+        <footer>
+          This projet is created by <a href="https://github.com/IraCannet">Ira</a> and is{" "}
+          <a href="https://github.com/IraCannet/cutemeteo-on-react" target="_blank">open-sourced on GitHub</a>
+        </footer>
+      </div>
   );
 }
