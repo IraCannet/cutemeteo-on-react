ea43671faa4b4afde1be68b12aa01d82fafcf20f
IraCannet <58562850+IraPapsheva@users.noreply.github.com>
2020-12-26 17:35:04 +0100

    problem-with-footer

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-        <footer>
-          This projet is created by <a href="https://github.com/IraCannet">Ira</a> and is{" "}
-          <a href="https://github.com/IraCannet/cutemeteo-on-react" target="_blank">open-sourced on GitHub</a>
-        </footer>
-      </div>
+
+        	      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/IraCannet" target="_blank">
+          Ira
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/IraCannet/cutemeteo-on-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
   );
 }
