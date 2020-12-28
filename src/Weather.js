83c93eba6a9b0917ad30500f2451bd77e6b10759
IraCannet <58562850+IraPapsheva@users.noreply.github.com>
2020-12-28 15:09:51 +0100

    axios-added-integrations-apiweather

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import "./Weather.css";
 
 export default function Weather() {
+    const[ready, setReady]= useState(false);
+    const[weatherData, setWeatherData]= useState({});
+    const [temperature, setTemperature] = useState(null);
+    
+    function handleResponse(response){
+        console.log(response.data);
+        setWeatherData({
+            temperature: response.data.main.temp,
+            wind: response.data.wind.speed,
+            city: response.data.name
+        });
+        setReady(true);
+    }
+    
+    if(ready){
+
     return(
         <div className="Weather">
             <form>
@@ -14,10 +31,10 @@ export default function Weather() {
                     </div>
                 </div>
             </form>
-            <h1>Paris</h1>
+            <h1>{weatherData.city}</h1>
             <ul>
                 <li>Wednesday 07:00</li>
-                <li>Mostly cloudy</li>
+                <li>{weatherData.description}</li>
             </ul>
             <div className="row mt-3"> 
                 <div className="col-6">
@@ -28,19 +45,29 @@ export default function Weather() {
                             className="float-left"
                             />
                         <div className="float-left">
-                            <span className="temperature">6</span>
+                            <span className="temperature">{Math.round(weatherData.temperature)}</span>
                             <span className="unit">°C</span>
                         </div>
                     </div>
-                </div>
+                </div> 
                 <div className="col-6">
                     <ul>
                         <li>Precipitation: 15%</li>
                         <li>Humidity: 73%</li>
-                        <li>Wind: 13 km/h</li>
+                        <li>Wind: {weatherData.wind} km/h</li>
                     </ul>
                 </div>
             </div>
         </div>
-    )
+    );
+    } else{
+        const apiKey = "c4ec87540b7d8d71553d9dad178f26b1";
+        let units = "metric";
+        let city = "Paris";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
+
+        return "Loading..";
+    }
 }
