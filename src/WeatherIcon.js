9107542e6731a14058818348d41b9e58d3b22896
IraCannet <58562850+IraPapsheva@users.noreply.github.com>
2021-01-06 22:41:49 +0100

    animated-icons-added

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,20 +1,34 @@
 import React from "react";
-import { WeatherThemeProvider, Sunny, Cloudy, Rain, Snow } from 'weather-styled-icon';
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
-  const theme = {
-  backgroundColor: 'Lavender',
-  sunColor: 'blue',
-  raysColor: 'OrangeRed',
-};
- 
+  const codeMapping = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "PARTLY_CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "RAIN",
+    "09n": "RAIN",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG"
+  };
 
-
-    return(
-  <div>
-  <WeatherThemeProvider theme={theme}>
-    <Sunny />
-  </WeatherThemeProvider>
-  </div>
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="#ffc7c7"
+      size={52}
+      animate={true}
+    />
   );
 }
